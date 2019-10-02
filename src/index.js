704954491392267c7fc64c60c73239e8a9176e7f
Alex <alekseys.workemail@gmail.com>
2019-10-02 22:29:08 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@ function eval() {
     // Do not use eval!!!
     return;
 }
+function myeval(expr) {
+    if(expr.indexOf('/ 0')>0){
 
-function expressionCalculator(expr) {
-    // write your solution here
+        throw 'TypeError: Division by zero.'
+    }
+    try {
+        return new Function('return ' + expr);
+    } catch (e) {
+        if (e.toString().indexOf("Unexpected token")) {
+            throw "ExpressionError: Brackets must be paired";
+        }
+        throw e;
+    }
 }
 
+
+function expressionCalculator(expr) {
+    let temp = myeval(expr);
+    return temp();
+}
+// expressionCalculator('1 + 2) * 3')
 module.exports = {
     expressionCalculator
 }
